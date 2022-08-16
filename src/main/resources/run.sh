b7b39ccca95cd057f2bb6334ccca6200dd514183
muieer <hhyforsub@icloud.com>
2022-08-16 19:08:26 +0800

    up

---
+++ src/main/resources/run.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+spark-submit --class org.muieer.study.sql.ProcessDataFrameByFunctionDemo \
+            /Users/yangdahu/code/spark_study/target/spark_study-1.0-SNAPSHOT-jar-with-dependencies.jar
