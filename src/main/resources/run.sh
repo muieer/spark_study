e52aad0ef94cab82de2c6dfa9ebf5e773f636d6b
muieer <yang_for_work@qq.com>
2022-10-12 16:47:49 +0800

    up

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 
-spark-submit --class org.muieer.study.sql.ProcessDataFrameByFunctionDemo \
+mainClass=org.muieer.study.sql.ProcessDataFrameByFunctionDemo
+spark-submit --class $mainClass \
             /Users/yangdahu/code/spark_study/target/spark_study-1.0-SNAPSHOT-jar-with-dependencies.jar
